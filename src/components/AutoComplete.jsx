85a8eac0b4f7a998c73c486f10a1d9aef907d160
Oscar Norman <oscarnormn@gmail.com>
2023-10-12 16:53:14 +0200

    Fix

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -3,7 +3,6 @@ import { Typeahead } from 'react-bootstrap-typeahead';
 
 import studentData from '../json/studentData.json';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
 
