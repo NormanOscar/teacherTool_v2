490be2ddbd78956ef94d4e184aaa6d2d82a80abf
Oscar Norman <oscarnormn@gmail.com>
2023-11-07 16:44:48 +0100

    Ändrat bredd på konfimeringsruta

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -8,14 +8,14 @@ export default function ConfirmationModal({ show, closeModal, onDeny }) {
   }
 
   return (
-    <Modal show={show} onHide={closeModal} centered>
-        <Modal.Body>
-            <p>Vill du verkligen avsluta interventionen?</p>
-        </Modal.Body>
-        <Modal.Footer>
-            <button type="button" className="btn btn-secondary" onClick={closeModal}>Avbryt</button>
-            <button type="button" className="btn btn-danger" onClick={onAccept}>Avsluta</button>
-        </Modal.Footer>
+    <Modal dialogClassName="confirmationModal" show={show} onHide={closeModal} centered>
+      <Modal.Body>
+        <p>Vill du verkligen avsluta interventionen?</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <button type="button" className="btn btn-secondary" onClick={closeModal}>Avbryt</button>
+        <button type="button" className="btn btn-danger" onClick={onAccept}>Avsluta</button>
+      </Modal.Footer>
     </Modal>
   );
 }
