5ce46c56a7f8336178bab96e380d4b0efca55f39
Oscar Norman <oscarnormn@gmail.com>
2023-12-21 14:30:54 +0100

    Admin kan lägga till nya användare

---
+++ src/components/AddNewUser.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import { Col, Card, Modal } from "react-bootstrap";
+
+export default function AddNewUser({ show, onClose }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState("");
+  const [inputResult, setInputResult] = useState({ msg: null, type: null });
+
+  const handleTextChange = (e) => {
+    switch (e.target.id) {
+      case "firstName":
+        setFirstName(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      case "lastName":
+        setLastName(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      case "email":
+        setEmail(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      case "username":
+        setUsername(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      case "password":
+        setPassword(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      case "confirmationPassword":
+        setConfirmationPassword(e.target.value);
+        setInputResult({ msg: null, type: null });
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName === "") {
+      setInputResult({ msg: "Förnamn saknas", type: "danger" });
+      return;
+    } else if (lastName === "") {
+      setInputResult({ msg: "Efternamn saknas", type: "danger" });
+      return;
+    } else if (email === "") {
+      setInputResult({ msg: "Email saknas", type: "danger" });
+      return;
+    } else if (username === "") {
+      setInputResult({ msg: "Användarnamn saknas", type: "danger" });
+      return;
+    } else if (password === "") {
+      setInputResult({ msg: "Lösenord saknas", type: "danger" });
+      return;
+    } else if (confirmationPassword === "") {
+      setInputResult({ msg: "Bekräfta lösenord saknas", type: "danger" });
+      return;
+    } else if (password !== confirmationPassword) {
+      setInputResult({ msg: "Lösenorden matchar inte", type: "danger" });
+      return;
+    } else {
+      let users = JSON.parse(localStorage.getItem("userData"));
+      let sortedUsers = users.sort((a, b) => a.id - b.id);
+      let id = sortedUsers[sortedUsers.length - 1].id + 1;
+      let data = {
+        id: id,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username,
+        password: password,
+      };
+      users.push(data);
+      localStorage.setItem("userData", JSON.stringify(users));
+      setInputResult({ msg: "Användare tillagd", type: "success" });
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setUsername("");
+      setPassword("");
+      setConfirmationPassword("");
+    }
+
+    window.location.reload(false);
+  }
+
+  return(
+    <Modal show={show} onHide={onClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Lägg till ny användare
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Col>
+          <form>
+            {inputResult.msg !== null && (
+              <div 
+              className={
+                inputResult.type === "danger"
+                  ? "alert alert-danger"
+                  : "alert alert-success"
+              } role="alert">
+                {inputResult.msg}
+              </div>
+            )}
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="firstName">
+                Förnamn <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                value={firstName}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="lastName">
+                Efternamn <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                value={lastName}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="email">
+                Email <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="username">
+                Användarnamn <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="password">
+                Lösenord <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="text"
+                id="password"
+                value={password}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="form-outline mb-2">
+              <label style={{ margin: 0 }} htmlFor="confirmationPassword">
+                Godkänn lösenord <span className="required-symbol">*</span>
+              </label>
+              <input
+                type="text"
+                id="confirmationPassword"
+                value={confirmationPassword}
+                className="form-control"
+                onChange={handleTextChange}
+              />
+            </div>
+            <div className="mb-2 d-flex justify-content-center">
+              <button className="btn btn-primary" onClick={handleSubmit}>
+                Lägg till
+              </button>
+            </div>
+          </form>
+          </Col>
+        </Modal.Body>
+    </Modal>
+  )
+}
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Card, Modal } from "react-bootstrap";
 import { activities } from "../json/activities.json";
+import AddNewStudent from "../components/AddNewStudent";
+import AddNewUser from "../components/AddNewUser";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
@@ -8,16 +10,15 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 library.add(faTrash);
 
 export default function Admin() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [selectedGrade, setSelectedGrade] = useState("0");
-  const [inputResult, setInputResult] = useState({ msg: null, type: null });
-  
+  const [newUser, setNewUser] = useState(false);
+  const showNewUserModal = () => setNewUser(true);
+  const hideNewUserModal = () => setNewUser(false);
+
+  let users = JSON.parse(localStorage.getItem('userData'));
+
   useEffect(() => {
     if (localStorage.getItem('login')) {
       let userId = JSON.parse(localStorage.getItem('userId'));
-      let users = JSON.parse(localStorage.getItem('userData'));
       let user = users.find(user => user.id === userId);
       if (user.role !== "admin") {
         window.location.href = '/';
@@ -27,75 +28,15 @@ export default function Admin() {
     }
   }, []);
 
-  const handleSelectChange = (e) => {
-    setSelectedGrade(e.target.value);
-    setInputResult({ msg: null, type: null });
-  };
+  const removeUser = (e) => {
+    let userId = e.currentTarget.id;
+    let users = JSON.parse(localStorage.getItem('userData'));
+    let userIndex = users.findIndex(user => user.id === userId);
+    users.splice(userIndex, 1);
 
-  const handleTextChange = (e) => {
-    switch (e.target.id) {
-      case "firstName":
-        setFirstName(e.target.value);
-        setInputResult({ msg: null, type: null });
-        break;
-      case "lastName":
-        setLastName(e.target.value);
-        setInputResult({ msg: null, type: null });
-        break;
-      case "email":
-        setEmail(e.target.value);
-        setInputResult({ msg: null, type: null });
-        break;
-      default:
-        break;
-    }
-  }
+    localStorage.setItem('userData', JSON.stringify(users));
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (firstName === "") {
-      setInputResult({ msg: "Förnamn saknas", type: "danger" });
-      return;
-    } else if (lastName === "") {
-      setInputResult({ msg: "Efternamn saknas", type: "danger" });
-      return;
-    } else if (email === "") {
-      setInputResult({ msg: "Email saknas", type: "danger" });
-      return;
-    } else if (selectedGrade === "0") {
-      setInputResult({ msg: "Årskurs saknas", type: "danger" });
-      return;
-    }
-
-    let students = JSON.parse(localStorage.getItem('studentData'));
-
-    if (students.find(student => student.email === email)) {
-      setInputResult({ msg: "Email finns redan", type: "danger" });
-      return;
-    }
-
-    let sortedStudents = students.sort((a, b) => a.id - b.id);
-    let id = sortedStudents[sortedStudents.length - 1].id + 1;
-
-    let data = {
-      id : id,
-      name : firstName + " " + lastName,
-      grade : Number(selectedGrade),
-      email : email,
-      assessments : [],
-      activeActivities : 0,
-      cancelledActivities : 0,
-      activities : [],
-    }
-
-    students.push(data);
-    localStorage.setItem('studentData', JSON.stringify(students));
-    setInputResult({ msg: "Elev tillagd", type: "success" });
-    setSelectedGrade("0");
-    setFirstName("");
-    setLastName("");
-    setEmail("");
+    window.location.reload(false);
   }
   
   return (
@@ -105,11 +46,60 @@ export default function Admin() {
           <Col xs={12} md={4} style={{paddingTop: '2em'}}>
             <Card className="p-4 my-2">
               <h4 className="mb-4" style={{ textAlign: "center" }}>
+                Användare
+              </h4>
+              <ul className="list-group list-group-flush overflow-auto custom-scrollbar" style={{ maxHeight: "300px" }}>
+                {users && users.map((user, index) => {
+                  return(
+                    <li 
+                      key={index}
+                      className="list-group-item"
+                    >
+                      <Row>
+                        <Col>
+                          <p className="mb-0 d-flex justify-content-between">
+                            {user.firstName + " " + user.lastName}
+                          </p>
+                          <p className="mb-0">{user.email}</p>
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-end">
+                          <FontAwesomeIcon icon={faTrash} size="lg" id={user.id} onClick={removeUser} />
+                        </Col>
+                      </Row>
+                    </li>
+                  )
+                })}
+              </ul>
+              <Col className="d-flex justify-content-center mt-4 ">
+                <button className="btn btn-primary w-50" onClick={showNewUserModal}>
+                  Lägg till användare
+                </button>
+              </Col>
+            </Card>
+            { newUser && (
+              <AddNewUser
+                show={newUser}
+                onClose={hideNewUserModal}
+              />
+            )}
+          </Col>
+          <Col xs={12} md={4} style={{paddingTop: '2em'}}>
+            <Card className="p-4 my-2">
+              <h4 className="mb-4" style={{ textAlign: "center" }}>
+                Admin
+              </h4>
+              <p>Här kan du som admin lägga till nya elever, aktiviteter samt bedömngsmaterial.</p>
+            </Card>
+          </Col>
+          <Col xs={12} md={4} style={{paddingTop: '2em'}}>
+            <AddNewStudent />
+            <Card className="p-4 my-2">
+              <h4 className="mb-4" style={{ textAlign: "center" }}>
                 Aktiviteter
               </h4>
               <ul className="list-group list-group-flush overflow-auto custom-scrollbar" style={{ maxHeight: "300px" }}>
                 {activities.map((activity, index) => {
-                  return (
+                  return(
                     <li 
                       key={index}
                       className="list-group-item"
@@ -126,99 +116,13 @@ export default function Admin() {
                         </Col>
                       </Row>
                     </li>
-                  );
+                  )
                 })}
               </ul>
               <Col className="d-flex justify-content-center mt-4 ">
                 <button className="btn btn-primary w-50">
                   Lägg till aktivitet
                 </button>
-              </Col>
-            </Card>
-          </Col>
-          <Col xs={12} md={4} style={{paddingTop: '2em'}}>
-            <Card className="p-4 my-2">
-              <h4 className="mb-4" style={{ textAlign: "center" }}>
-                Admin
-              </h4>
-              <p>Här kan du som admin lägga till nya elever, aktiviteter samt bedömngsmaterial.</p>
-            </Card>
-          </Col>
-          <Col xs={12} md={4} style={{paddingTop: '2em'}}>
-            <Card className="p-4 my-2">
-              <h4 className="mb-4" style={{ textAlign: "center" }}>
-                Lägg till elev
-              </h4>
-              <Col className="mt-4 ">
-              <form>
-                {inputResult.msg !== null && (
-                  <div 
-                  className={
-                    inputResult.type === "danger"
-                      ? "alert alert-danger"
-                      : "alert alert-success"
-                  } role="alert">
-                    {inputResult.msg}
-                  </div>
-                )}
-                <div className="form-outline mb-2">
-                  <label style={{ margin: 0 }} htmlFor="firstName">
-                    Förnamn <span className="required-symbol">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    id="firstName"
-                    value={firstName}
-                    className="form-control"
-                    onChange={handleTextChange}
-                  />
-                </div>
-                <div className="form-outline mb-2">
-                  <label style={{ margin: 0 }} htmlFor="lastName">
-                    Efternamn <span className="required-symbol">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    id="lastName"
-                    value={lastName}
-                    className="form-control"
-                    onChange={handleTextChange}
-                  />
-                </div>
-                <div className="form-outline mb-2">
-                  <label style={{ margin: 0 }} htmlFor="email">
-                    Email <span className="required-symbol">*</span>
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    className="form-control"
-                    onChange={handleTextChange}
-                  />
-                </div>
-                <div className="form-outline mb-2">
-                  <label style={{ margin: 0 }} htmlFor="grade">
-                    Årskurs <span className="required-symbol">*</span>
-                  </label>
-                  <select
-                    className="form-select"
-                    type="text"
-                    id="grade"
-                    value={selectedGrade}
-                    onChange={handleSelectChange}>
-                    <option value="0">Välj årskurs</option>
-                    <option value="1">Årskurs 1</option>
-                    <option value="2">Årskurs 2</option>
-                    <option value="3">Årskurs 3</option>
-                    </select>
-                </div>
-                <div className="mb-2 d-flex justify-content-center">
-                  <button className="btn btn-primary" onClick={handleSubmit}>
-                    Lägg till
-                  </button>
-                </div>
-              </form>
               </Col>
             </Card>
           </Col>
