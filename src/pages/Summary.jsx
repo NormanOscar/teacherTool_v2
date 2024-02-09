80e799ee8e4f769a181e2dd538a2f0072098b82b
Oscar Norman <oscarnormn@gmail.com>
2024-02-09 09:12:35 +0100

    Exportera till pdf

--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -1,12 +1,20 @@
-import { useState, useEffect } from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Container, Dropdown } from "react-bootstrap";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 library.add(faFilePdf);
 
+import MyDocument from "../components/Summary/MyDocument";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
 export default function Summary() {
+  const [loader, setLoader] = useState(false);
+  const [student, setStudent] = useState({});
+
   useEffect(() => {
     if (
       !localStorage.getItem("login") ||
@@ -16,11 +24,28 @@ export default function Summary() {
       window.location.href = "/login";
     }
 
-    const student = JSON.parse(localStorage.getItem("studentData")).find(
+    setStudent(JSON.parse(localStorage.getItem("studentData")).find(
       (student) => student.id === JSON.parse(localStorage.getItem("studentId"))
-    );
-    console.log(student);
+    ));
   }, []);
+  console.log(student);
+
+  const handlePrint = (mode) => {
+    const documentToPrint = document.querySelector("#document-to-print");
+    console.log(documentToPrint);
+    setLoader(true);
+    html2canvas(documentToPrint).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF(mode, "mm", "a4");
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const imgWidth = pageWidth;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      setLoader(false);
+      const documentName = `Sammanställning_${student.name}_${new Date().toLocaleDateString()}`;
+      pdf.save(`${documentName}.pdf`);
+    });
+  };
 
   return (
     <>
@@ -52,13 +77,35 @@ export default function Summary() {
             md={2}
             className="d-flex justify-content-end align-items-center"
           >
-            <button
-              className="btn btn-primary py-3 px-4"
-              onClick={() => console.log("Exportera")}
-            >
+            <Dropdown>
+              <Dropdown.Toggle className="btn btn-primary py-3 px-4">
+                <span className="mx-2">Exportera till PDF</span>
+                <FontAwesomeIcon size="xl" icon="fas fa-file-pdf" />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="text-center" style={{width: '100%'}}>
+                <Dropdown.Item onClick={() => {console.log('Förhandsgranska');}}>
+                  Förhandsgranska
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={() => handlePrint("p")}>
+                  Porträtt
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={() => handlePrint("l")}>
+                  Landskap
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            {/* <button className="btn btn-primary py-3 px-4" onClick={handlePrint}>
               <span className="mx-2">Exportera till PDF</span>
               <FontAwesomeIcon size="xl" icon="fas fa-file-pdf" />
-            </button>
+            </button> */}
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center align-items-center">
+          <Col xs={12} md={8} className="text-center">
+            <MyDocument id="document-to-print" student={student} />
           </Col>
         </Row>
       </Container>
